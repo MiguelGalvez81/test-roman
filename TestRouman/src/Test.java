8d89ff6a252da1167a7a7592629110944ee1b216
Miguel Galvez <miguel_galvez81@hotmail.com>
2024-02-07 16:36:52 -0600

    First digit thousand

--- TestRouman/src/Test.java
+++ TestRouman/src/Test.java
@@ -3,11 +3,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		
+		System.out.println(getRomanNumber(1999));
 
 	}
 
-
-	public String getRomanNumber (Integer number) {
+	//number = 2021
+	public static String getRomanNumber (Integer number) {
 		
 		String result = "";
 	
@@ -15,16 +16,31 @@ public class Test {
 			
 			String unit = "I";
 			String decimal = "X";
+			String fifty = "L";
 			String centimal = "C";
+			String fiveHundred = "D";
 			String thousand = "M";
+			
 			
 			String numb = String.valueOf(number.intValue());
 			
 			if(numb.length() > 3) {
 				String first = numb.substring(0,1);
 				
+				//2
+				for (int i = 0; i < Integer.parseInt(first); i++) {
+					result += thousand;
+				}
 				
-				result += thousand;
+				String second = numb.substring(1,2);
+				
+			
+				
+				
+				
+
+				
+				
 				
 				
 				
