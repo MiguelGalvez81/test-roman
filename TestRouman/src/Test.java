43d1e5f65b46ada184b604afcf965a530f99a596
Miguel Galvez <miguel_galvez81@hotmail.com>
2024-02-07 19:11:26 -0600

    Refactor funtion to repeat characters. Second digit centimals

--- TestRouman/src/Test.java
+++ TestRouman/src/Test.java
@@ -24,35 +24,51 @@ public class Test {
 			
 			String numb = String.valueOf(number.intValue());
 			
-			if(numb.length() > 3) {
-				String first = numb.substring(0,1);
-				
-				//2
-				for (int i = 0; i < Integer.parseInt(first); i++) {
-					result += thousand;
-				}
-				
-				String second = numb.substring(1,2);
-				
-				
+			char [] chars = numb.toCharArray();
 			
+			for (int i = chars.length; i > 0; i--) {
 				
+				Integer digit = 0;
+				if(i > 3) 
+					result += repeatChars(thousand, Integer.parseInt(numb.substring(0,1)));
 				
-				
-
-				
-				
-				
-				
-				
-				
+				if(i == 3 && chars.length > 3) { 
+					digit = Integer.parseInt(numb.substring(1,2));
+					
+					if(digit.intValue() > 0 && digit.intValue() < 4)
+						result += repeatChars(decimal, digit);
+					else if(digit.intValue() == 4)
+						result += repeatChars(centimal, 1);
+					else if(digit.intValue() == 5)
+						result += fiveHundred;
+					else if(digit.intValue() > 5 && digit.intValue() < 9)
+						result += fiveHundred + repeatChars(centimal, digit - 5);
+					else if(digit.intValue() == 9)
+						result += centimal + thousand;
+						
+				} else if (i == 3 && chars.length == 3) { 
+					digit = Integer.parseInt(numb.substring(0,1));
+					result = repeatChars(thousand, digit);
+				}
 			}
 			
 			
+			
+			
+					
 		}
 		
 		return result;	
 	
+	}
+	
+	public static String repeatChars(String character, Integer times) {
+		String result = "";
+		
+		for (int i = 0; i < times; i++) {
+			result += character;
+		}
+		return result;
 	}
 	
 }
